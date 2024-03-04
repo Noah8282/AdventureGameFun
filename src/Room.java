4b1a01cc91701070a3b63e7d4ad0be3d8715c2ac
Noah8282 <nova0001@stud.kea.dk>
2024-03-04 11:56:37 +0100

    Creation of Adventure, Enemy, Item, Userinterface and Room. Creation of Room Attributes.

---
+++ src/Room.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Room {
+    /////// ATTRIBUTES ///////
+
+    //coordinates
+    private int x;
+    private int y;
+
+    //Descriptive Room Attributes
+    private String name;
+    private String description;
+
+    //Items in room
+    ArrayList<Item> items;
+
+    //Enemies in room
+    ArrayList<Enemy> enemies;
+
+
+    public Room(int x, int y, String name, String description) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.description = description;
+    }
+}
